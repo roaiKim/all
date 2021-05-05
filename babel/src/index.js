9ba52d406bd67f74bece26bd2a6f2dd469e2752a
roaiKim <shcroai@gmail.com>
2021-05-05 22:36:53 +0800

    daily update

---
+++ babel/src/index.js
@@ -0,0 +1,10 @@
+// import "@babel/polyfill"
+const fn = () => {
+    console.log("a")
+}
+
+const isHas = [1,2,3,4].includes(2);
+
+const pro = new Promise((resolve, reject) => {
+    resolve(100)
+})
