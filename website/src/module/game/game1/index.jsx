44aa291375d07a0e9233b57321c3ea108e60186b
roaiKim <shcroai@gmail.com>
2021-05-25 16:16:15 +0800

    update

--- website/src/module/game/game1/index.jsx
+++ website/src/module/game/game1/index.jsx
@@ -1,8 +1,8 @@
-import { Lifecycle, Module, register } from 'core';
-import Main from './component';
+import { Lifecycle, Module, register } from "core";
+import Main from "./component";
 
 const initialState = {
-  
+
 };
 
 class Game1Module extends Module {
@@ -12,6 +12,6 @@ class Game1Module extends Module {
   }
 }
 
-const module = register(new Game1Module('game1', initialState));
+const module = register(new Game1Module("game1", initialState));
 export const actions = module.getActions();
 export const MainComponent = module.attachLifecycle(Main);
