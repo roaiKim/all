a217d264cce5a01a5b6de3685bb1d836c2272139
roaiKim <shcroai@gmail.com>
2021-12-01 18:38:44 +0800

    update

--- lea-npm/src/npm/redu/index.js
+++ lea-npm/src/npm/redu/index.js
@@ -1,25 +1,13 @@
 import Address from "./address.json";
 const getAddressGrade = (list, grade) => {
     const fun = (children, index) => {
-        // index.value = index.value + 1;
-        let config = true;
-        if (index + 1 >= grade) {
-            return null;
-        }
-        if (!children?.length) {
-            return null;
-        }
-        return children.map((item) => {
-            let chs = null;
-            if (config && item.children?.length) {
-                chs = fun(item.children, index + 1)
-            }
-            return {
-                label: item.label,
-                value: item.value,
-                children: chs, // item.children?.length && index < grade ? fun(item.children) : null,
-            };
-        });
+        return index >= grade || !children?.length
+            ? null
+            : children.map((item) => ({
+                  label: item.label,
+                  value: item.value,
+                  children: (item.children?.length && fun(item.children, index + 1)) || null,
+              }));
     };
     return list.map((item) => {
         const index = 0;
@@ -30,4 +18,4 @@ const getAddressGrade = (list, grade) => {
         };
     });
 };
-console.log(getAddressGrade(Address.data, 3));
+console.log(getAddressGrade(Address.data, 2));
