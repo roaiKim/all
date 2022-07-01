a20ffc67a6a07073228797d0be47705840b17c9b
roaiKim <shcroai@gmail.com>
2022-07-01 19:09:37 +0800

    [fix]: 更新

--- ci/src/module/common/header/component/HeaderTab.tsx
+++ ci/src/module/common/header/component/HeaderTab.tsx
@@ -1,48 +1,39 @@
 import { CloseOutlined } from "@icon";
-import { arrayMoveImmutable } from "array-move";
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
 
 interface HeaderTabProps {
-    // name: number;
-    // isActive: boolean;
-    // onClick: () => void;
-    value: any;
+    name: number;
+    isActive: boolean;
+    onClick: () => void;
 }
 
-export function HeaderTab(props: HeaderTabProps) {
-    // const { name, isActive, onClick } = props;
-    const { value } = props;
-    const isActive = false;
+export const SortableItem = SortableElement((props: HeaderTabProps) => {
+    const { name, isActive, onClick } = props;
     return (
-        <div title={`${name}`} className={`ro-header-tab-item ro-flex ${isActive ? "active" : ""}`} onClick={() => {}}>
-            <div className="ro-tab-trapezoid"></div>
-            <span className="ro-tab-title">项理{value}</span>
+        <div title={`${name}`} className={`ro-header-tab-item-g ro-flex ${isActive ? "active" : ""}`}>
+            <div className="ro-tab-trapezoid" onClick={onClick}></div>
+            <span className="ro-tab-title" onClick={onClick}>
+                项理{name}
+            </span>
             <CloseOutlined />
-        </div>
-    );
-}
-
-export const SortableItem = SortableElement(({ value }) => <HeaderTab value={value} />);
-
-export const SortableList = SortableContainer(({ items }) => {
-    return (
-        <div className="ro-header-tabs ro-flex ro-col-center">
-            {items.map((item, index) => (
-                <SortableItem key={`item-${item}`} index={index} value={item} />
-            ))}
         </div>
     );
 });
 
-export function SortableComponent() {
-    const [tabs, setTabs] = useState<string[]>(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]);
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        setTabs((prevTabs) => {
-            const a = arrayMoveImmutable(prevTabs, oldIndex, newIndex);
-            console.log("--items--", prevTabs, a, oldIndex, newIndex);
-            return a;
-        });
-    };
-    return <SortableList axis="x" lockAxis="x" items={tabs} onSortEnd={onSortEnd} hideSortableGhost={true} />;
+interface SortableTabsProps {
+    tabs: any[];
+    activeKey: string;
+    onClick: Dispatch<SetStateAction<string>>;
 }
+
+export const SortableTabs = SortableContainer((props: SortableTabsProps) => {
+    const { tabs, onClick, activeKey } = props;
+    return (
+        <div className="ro-header-tabs ro-flex ro-col-center">
+            {tabs.map((item, index) => (
+                <SortableItem key={`item-${item}`} isActive={activeKey === item} name={item} index={index} onClick={() => onClick(item)} />
+            ))}
+        </div>
+    );
+});
--- ci/src/module/common/header/component/index.tsx
+++ ci/src/module/common/header/component/index.tsx
@@ -1,23 +1,36 @@
+import { arrayMoveImmutable } from "array-move";
 import React, { useState } from "react";
-import { arrayMove } from "react-sortable-hoc";
-import { SortableList, HeaderTab, SortableComponent } from "./HeaderTab";
+import { SortableTabs } from "./HeaderTab";
 import "./index.less";
 
 export default function () {
-    const [activeKey, setActiveKey] = useState<number>();
+    const [activeKey, setActiveKey] = useState<string>();
     const [tabs, setTabs] = useState<string[]>(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]);
+
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        if (oldIndex === newIndex) return;
+        setTabs((prevTabs) => arrayMoveImmutable(prevTabs, oldIndex, newIndex));
+    };
+
     return (
         <header className="ro-header-container ro-flex">
             <div className="ro-header-logo ro-flex ro-col-center">
                 <img alt="logo" src={localStorage.getItem("headerLogo")} />
                 <div className="text-overflow-ellipsis">中集冷云综合服务平台</div>
             </div>
-            {/* <div className="ro-header-tabs ro-flex ro-col-center">
-                {[1, 3, 4, 5, 6].map((item) => (
-                    <HeaderTab key={item} isActive={activeKey === item} name={item} onClick={() => setActiveKey(item)} />
-                ))}
-            </div> */}
-            <SortableComponent />
+            <SortableTabs
+                axis="x"
+                lockAxis="x"
+                lockOffset="0%"
+                lockToContainerEdges
+                hideSortableGhost
+                distance={10}
+                onSortEnd={onSortEnd}
+                helperClass="ro-header-tab-help-sort"
+                tabs={tabs}
+                activeKey={activeKey}
+                onClick={setActiveKey}
+            />
             <div className="ro-header-operate"></div>
         </header>
     );
