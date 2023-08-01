ae22fee5518bca39923a14e6d066e46a29582e96
roaiKim <shcroai@gmail.com>
2023-08-01 17:15:11 +0800

    [refactor]: 打包生成构建文件

--- wechat-app/config/prod.js
+++ wechat-app/config/prod.js
@@ -3,7 +3,23 @@ module.exports = {
         NODE_ENV: '"production"',
     },
     defineConstants: {},
-    mini: {},
+    mini: {
+        /**
+     * WebpackChain 插件配置
+     * @docs https://github.com/neutrinojs/webpack-chain
+     */
+    webpackChain (chain) {
+        /**
+         * 如果 h5 端编译后体积过大，可以使用 webpack-bundle-analyzer 插件对打包体积进行分析。
+         * @docs https://github.com/webpack-contrib/webpack-bundle-analyzer
+         */
+        chain.plugin('analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [{
+            analyzerMode: "static", // 生成 HTML 的方式
+            openAnalyzer: false,
+        }])
+      }
+    },
     h5: {
         /**
          * WebpackChain 插件配置
