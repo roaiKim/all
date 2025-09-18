ed1065d16902c60bad4729c17166f6b76326f010
roaikim <shcroai@gmail.com>
2025-09-18 11:02:40 +0800

    feat: 新增stylelint格式校验

---
+++ npm-package/stylelint-config-base/index.js
@@ -0,0 +1,8 @@
+ module.exports = {
+  "extends": "stylelint-config-standard",
+  "customSyntax": "postcss-less",
+  "rules": {
+    "no-descending-specificity": null,
+    "no-invalid-double-slash-comments": null
+  }
+}
